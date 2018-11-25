4ea83e6fa835a37afb8adc1edeb445f4a390f800
Natasha <natasha.pechenka@gmail.com>
2018-11-25 21:14:01 +0600

    work solution as I think

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // assignment 1
-var money = +prompt("Ваш бюджет на месяц?");
+var money = prompt("Ваш бюджет на месяц?");
 var time = prompt("Введите дату в формате YYYY-MM-DD");
 var expensesString = prompt("Введите обязательную статью расходов в этом месяце");
-var expensesNumber = +prompt("Во сколько обойдется?");
+var expensesNumber = prompt("Во сколько обойдется?");
 
 var appData = {
     budget: money,
     timeData: time,
-    expenses: expensesString + " " + expensesNumbe,
-    optionalExpenses:,
+    expenses: expensesString + " : " + expensesNumber,
+    optionalExpenses:undefined,
     income: [],
     savings: false
 }
 
-alert(appData);
+alert(
+    "Итак, месячный бюджет: " + appData.budget + 
+    ". С учётом " + appData.expenses +
+    ". Ваш ежедневный бюджет составит: " +
+    ((money - (+expensesNumber))/30) + " рублей."
+);
